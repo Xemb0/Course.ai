bbeaa40eb9960b4e1c24cadbca462ac038f444b3
XEMBO <nishadvishal937@gamil.com>
2023-09-20 21:49:05 +0100

    Animated Navbar

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div class="Container">
+    <div class="Navbar">
+        <ul class="Nav_ul">
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Categories</li>
+            <li>About</li>
+        </ul>
+    </div>
+</div>
+  )
+}
+
+export default Navbar
